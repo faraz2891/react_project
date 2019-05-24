c23d52a04e69dd1187a4da89e4a6fd18577e102d
Syed Faraz Ali <sfali@Ingenuity.local>
2019-05-24 09:34:42 +0530

    updating the state from children component to parent component

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,13 +2,11 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 0,
-    tags: ["tag1", "tag2", "tag3"]
+    value: this.props.counters.value
   };
   handleIncrement(product) {
-    console.log("Increment Clicked", product);
     this.setState({
-      count: this.state.count + 1
+      value: this.state.value + 1
     });
   }
   render() {
@@ -21,18 +19,19 @@ class Counter extends Component {
         >
           Increment
         </button>
+        <button onClick={()=> this.props.onDelete(this.props.counters.id)} className="btn btn-danger btn-sm m-2">Delete</button>
       </div>
     );
   }
 
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
+    classes += this.state.value === 0 ? "warning" : "primary";
     return classes;
   }
 
   formatCount() {
-    const { count } = this.state;
+    const { value: count } = this.state;
     return count === 0 ? "Zero" : count;
   }
 }
--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -2,14 +2,23 @@ import React, { Component } from "react";
 import Counter from "./counter";
 
 class Counters extends Component {
-  state = {};
+  state = {
+    counters: [
+      {id: 1, value: 4},
+      {id: 2, value: 0},
+      {id: 3, value: 0},
+      {id: 4, value: 0}
+    ]
+  };
+
+  handleDelete = (counterId) => {
+    const counters = this.state.counters.filter(c=> c.id !== counterId);
+    this.setState({ counters })
+  }
   render() {
     return (
       <div>
-        <Counter />
-        <Counter />
-        <Counter />
-        <Counter />
+        {this.state.counters.map(counter => <Counter onDelete={this.handleDelete} key={counter.id} counters={counter} />)}
       </div>
     );
   }
